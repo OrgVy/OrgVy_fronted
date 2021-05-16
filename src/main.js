3a02bf590ca0146afab957c61ce8967e5c51f022
oalvarezr <oalvarezr@unal.edu.co>
2021-05-16 15:38:45 -0500

    first template

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import 'core-js/stable'
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import CoreuiVue from '@coreui/vue'
+import { iconsSet as icons } from './assets/icons/icons.js'
+import store from './store'
+
+axios.defaults.baseURL = 'http://localhost:3000/'
+
+Vue.config.performance = true
+Vue.use(CoreuiVue)
+Vue.prototype.$log = console.log.bind(console)
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  icons,
+  template: '<App/>',
+  components: {
+    App
+  }
+})
